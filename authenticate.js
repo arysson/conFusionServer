5ca5a9c6cbb27b68906aa2b01e38a37dba64a1ea
arysson <arysson.cavalcanti@gmail.com>
2020-06-29 14:42:23 -0300

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -2,6 +2,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var JwtStrategy = require('passport-jwt').Strategy;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var User = require('./models/users');
 var config = require('./config');
@@ -54,3 +55,32 @@ exports.verifyAdmin = (req, res, next) => {
         this.notAuthorizedError(next);
     }
 };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({
+        facebookId: profile.id
+    }, (err, user) => {
+        if (err) {
+            done(err);
+        } else if (user !== null) {
+            done(null, user);
+        } else {
+            user = new User({
+                username: profile.displayName
+            });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err) {
+                    done(err);
+                } else {
+                    done(null, user);
+                }
+            });
+        }
+    });
+}));
