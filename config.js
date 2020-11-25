460654c1ad4bd79ad96bcbc11ef9a6800c39bb90
arysson <arysson.cavalcanti@gmail.com>
2020-11-25 19:52:54 -0300

    Minor changes

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    secretKey: '12345-67890-09876-54321',
+    secretKey: 'MY_SECRET_KEY',
     mongoUrl: 'mongodb://localhost:27017/conFusion',
     facebook: {
-        clientId: '305299797270620',
-        clientSecret: '415d1d749adbc3d025524f8c80b98415'
+        clientId: 'MY_CLIENT_ID',
+        clientSecret: 'MY_CLIENT_SECRET'
     }
 };
